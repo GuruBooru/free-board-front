36b3584471c91d6fb2b311d917ac401b6f689c69
grapgrap <fussaep@gmail.com>
2018-03-25 20:29:47 +0900

    Merge branch 'read'
    # Conflicts:
    #	package-lock.json
    #	src/app/app-routing.module.ts
    #	src/app/app.component.html
    #	src/app/app.module.ts
    #	src/app/shared/service/post/post.service.ts

--- src/app/display/display.module.ts
+++ src/app/display/display.module.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { DisplayComponent } from './display.component';
-import { CommentComponent } from './comment/comment.component';
 import { CommentModule } from './comment/comment.module';
+import { DisplayComponent } from './display.component';
 import { ReadModule } from './read/read.module';
 
 @NgModule({
   imports: [
-    CommonModule, ReadModule, ReactiveFormsModule
     CommonModule,
+    ReadModule,
+    ReactiveFormsModule,
     CommentModule
   ],
   declarations: [DisplayComponent]
 })
-export class DisplayModule { }
+export class DisplayModule {
+}
